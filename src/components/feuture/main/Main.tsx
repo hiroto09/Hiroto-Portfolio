2cc1485c727b8b737ba904291680e44d8d4cab3a
hiroto09 <k23015kk@aitech.ac.jp>
2025-02-19 06:05:58 +0900

    一旦デプロイ用

--- src/components/feuture/main/Main.tsx
+++ src/components/feuture/main/Main.tsx
@@ -1,12 +1,11 @@
 import ThreeArea from "../three/ThreeArea";
 import {Title} from "../title/Title";
-import About from "../about/About";
 export default function Main ()  {
   return (
     <>
       <ThreeArea />
       <Title />
-      <About />
+
     </>
   );
 };

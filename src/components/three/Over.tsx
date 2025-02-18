98121a2cc6dea967f8f0dae340fec44f3c155805
hiroto09 <h16100929@icloud.com>
2025-02-18 23:54:54 +0900

    style:コンポーネント名の変更

--- src/components/three/Over.tsx
+++ src/components/three/Over.tsx
@@ -1,12 +1,11 @@
 
 import Three from './Three';
 import { Title } from '../title/Title';
+import { color } from 'three/tsl';
 
 function Overlay() {
   return (
-    <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', pointerEvents: 'none' }}>
       <Title />
-    </div>
   );
 }
 

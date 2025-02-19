2e73e318f155901ffb39db1b9c8fcd57f25ee188
hiroto09 <k23015kk@aitech.ac.jp>
2025-02-20 00:30:45 +0900

    fix:Aquariumの初期位置が真ん中に来るのを修正

--- src/components/feuture/three/Three.tsx
+++ src/components/feuture/three/Three.tsx
@@ -1,7 +1,13 @@
 "use client";
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { useMask, useGLTF, Float, Instance, Instances } from "@react-three/drei";
+import {
+  useMask,
+  useGLTF,
+  Float,
+  Instance,
+  Instances,
+} from "@react-three/drei";
 import {
   Lightformer,
   Environment,
@@ -35,12 +41,17 @@ interface OrcaProps {
 }
 
 export default function Three({ spheres }: AppProps) {
-  const [aquariumPosition, setAquariumPosition] = useState<[number, number, number]>([0, 0.25, 0]);
+  const [aquariumPosition, setAquariumPosition] = useState<
+    [number, number, number]
+  >([0, 0.25, 0]);
   const windowWidth = useRef<number | null>(null);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-      windowWidth.current = window.innerWidth;
+      
+      const newWidth = window.innerWidth;
+      setAquariumPosition(newWidth <= 1120 ? [0, 0.25, 0] : [0, 0.25, -8]);
+      windowWidth.current = newWidth;
 
       const handleResize = () => {
         const newWidth = window.innerWidth;
@@ -61,7 +72,10 @@ export default function Three({ spheres }: AppProps) {
   }, []);
 
   return (
-    <Canvas shadows camera={{ position: [30, 9, 0], fov: 35, near: 1, far: 40 }}>
+    <Canvas
+      shadows
+      camera={{ position: [30, 9, 0], fov: 35, near: 1, far: 40 }}
+    >
       <color attach="background" args={["white"]} />
       <Aquarium position={aquariumPosition}>
         <Float rotationIntensity={2} floatIntensity={10} speed={2}>
@@ -71,7 +85,13 @@ export default function Three({ spheres }: AppProps) {
           <sphereGeometry args={[1, 64, 64]} />
           <meshBasicMaterial depthTest={false} />
           {spheres.map(([scale, color, speed, position], index) => (
-            <Sphere key={index} scale={scale} color={color} speed={speed} position={position} />
+            <Sphere
+              key={index}
+              scale={scale}
+              color={color}
+              speed={speed}
+              position={position}
+            />
           ))}
         </Instances>
       </Aquarium>
@@ -84,16 +104,45 @@ export default function Three({ spheres }: AppProps) {
         scale={60}
         position={[0, -5, 0]}
       >
-        <RandomizedLight amount={8} radius={15} ambient={0.5} intensity={1} position={[-5, 10, -5]} size={20} />
+        <RandomizedLight
+          amount={8}
+          radius={15}
+          ambient={0.5}
+          intensity={1}
+          position={[-5, 10, -5]}
+          size={20}
+        />
       </AccumulativeShadows>
       <Environment resolution={1024}>
         <group rotation={[-Math.PI / 3, 0, 0]}>
-          <Lightformer intensity={4} rotation-x={Math.PI / 2} position={[0, 5, -9]} scale={[10, 10, 1]} />
+          <Lightformer
+            intensity={4}
+            rotation-x={Math.PI / 2}
+            position={[0, 5, -9]}
+            scale={[10, 10, 1]}
+          />
           {[2, 0, 2, 0, 2, 0, 2, 0].map((x, i) => (
-            <Lightformer key={i} form="circle" intensity={4} rotation={[Math.PI / 2, 0, 0]} position={[x, 4, i * 4]} scale={[4, 1, 1]} />
+            <Lightformer
+              key={i}
+              form="circle"
+              intensity={4}
+              rotation={[Math.PI / 2, 0, 0]}
+              position={[x, 4, i * 4]}
+              scale={[4, 1, 1]}
+            />
           ))}
-          <Lightformer intensity={2} rotation-y={Math.PI / 2} position={[-5, 1, -1]} scale={[50, 2, 1]} />
-          <Lightformer intensity={2} rotation-y={-Math.PI / 2} position={[10, 1, 0]} scale={[50, 2, 1]} />
+          <Lightformer
+            intensity={2}
+            rotation-y={Math.PI / 2}
+            position={[-5, 1, -1]}
+            scale={[50, 2, 1]}
+          />
+          <Lightformer
+            intensity={2}
+            rotation-y={-Math.PI / 2}
+            position={[10, 1, 0]}
+            scale={[50, 2, 1]}
+          />
         </group>
       </Environment>
     </Canvas>
@@ -118,15 +167,36 @@ function Aquarium({ children, ...props }: AquariumProps) {
 
   return (
     <group {...props} dispose={null}>
-      <mesh castShadow scale={[3.66, 4.8, 5]} geometry={(nodes.Cube as THREE.Mesh).geometry}>
-        <MeshTransmissionMaterial backside samples={4} thickness={1.5} chromaticAberration={0.025} anisotropy={0.1} distortion={0.1} distortionScale={0.1} temporalDistortion={0.2} iridescence={1} iridescenceIOR={1} iridescenceThicknessRange={[0, 1400]} />
+      <mesh
+        castShadow
+        scale={[3.66, 4.8, 5]}
+        geometry={(nodes.Cube as THREE.Mesh).geometry}
+      >
+        <MeshTransmissionMaterial
+          backside
+          samples={4}
+          thickness={1.5}
+          chromaticAberration={0.025}
+          anisotropy={0.1}
+          distortion={0.1}
+          distortionScale={0.1}
+          temporalDistortion={0.2}
+          iridescence={1}
+          iridescenceIOR={1}
+          iridescenceThicknessRange={[0, 1400]}
+        />
       </mesh>
       <group ref={ref}>{children}</group>
     </group>
   );
 }
 
-function Sphere({ position, scale = 1, speed = 0.1, color = "red" }: SphereProps) {
+function Sphere({
+  position,
+  scale = 1,
+  speed = 0.1,
+  color = "red",
+}: SphereProps) {
   return (
     <Float rotationIntensity={40} floatIntensity={20} speed={speed}>
       <Instance position={position} scale={scale} color={color} />

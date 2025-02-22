ce33d4d1c714b3a359774ea04c296c0022a721c6
hiroto09 <k23015kk@aitech.ac.jp>
2025-02-22 15:31:03 +0900

    add:一旦

--- src/components/feuture/main/projects/project-items/ProjectItems.tsx
+++ src/components/feuture/main/projects/project-items/ProjectItems.tsx
@@ -3,10 +3,7 @@ export default function ProjectItems() {
 
     return (
         <div className="content-title">
-        <h1>Projects</h1>
-            <div className="content">
             
-            </div>
         </div>
     )
 }

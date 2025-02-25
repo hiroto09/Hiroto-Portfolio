5ef34a9187e20ac89a8cc6b7bbdbb203176d2417
hiroto09 <k23015kk@aitech.ac.jp>
2025-02-25 23:31:36 +0900

    uppdate:一旦記事完成

--- src/app/(default_site)/post/[id]/page.tsx
+++ src/app/(default_site)/post/[id]/page.tsx
@@ -1,4 +1,5 @@
-import { client } from '@/libs/mcms/client';
+import Post from "@/components/feuture/post/Post"
+import { client } from "@/libs/mcms/client";
 
 interface Params {
   params: {
@@ -6,45 +7,13 @@ interface Params {
   };
 }
 
-interface PostType {
-  id: string;
-  title: string;
-  content: string;
-  eyecatch?: {
-    url: string;
-  };
+export default function Page({ params }: Params) {
+  return <Post id={params.id} />;
 }
 
-const Post = async ({ params }: Params) => {
-  const { id } = params; // URL パラメータから ID を取得
-  const data = await client.get({
-    endpoint: "blogs",
-  });
-
-  // 型を指定
-  const post: PostType | undefined = data.contents.find((post: PostType) => post.id === id);
-
-  if (!post) {
-    return <div>投稿が見つかりませんでした。</div>; // 投稿が見つからない場合の処理
-  }
-
-  return (
-    <main>
-      <h3>{post.title}</h3>
-      {post.eyecatch && (
-        <img src={post.eyecatch.url} alt={post.title} style={{ maxWidth: "100%", height: "auto" }} />
-      )}
-      <div dangerouslySetInnerHTML={{ __html: post.content }} />
-    </main>
-  );
-};
-
-// params の型を定義
 export const generateStaticParams = async () => {
   const data = await client.get({ endpoint: "blogs" });
-  return data.contents.map((post: PostType) => ({
+  return data.contents.map((post: { id: string }) => ({
     id: post.id,
   }));
 };
-
-export default Post;

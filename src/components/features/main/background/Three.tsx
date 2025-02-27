b19786fd274e733fb68e741b8ded12a8427a14c4
hiroto09 <k23015kk@aitech.ac.jp>
2025-02-28 06:48:33 +0900

    update:デザインの変更

--- src/components/features/main/background/Three.tsx
+++ src/components/features/main/background/Three.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useLayoutEffect, useRef, useState, useMemo } from "react";
-import { Canvas} from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import {
   useMask,
   useGLTF,
@@ -11,10 +11,12 @@ import {
   Environment,
   AccumulativeShadows,
   MeshTransmissionMaterial,
-  Text
+  Text,
+  useTexture,
 } from "@react-three/drei";
 
 import * as THREE from "three";
+import { url } from "inspector";
 
 // モデルのプリロード
 useGLTF.preload("/shapes-transformed.glb");
@@ -48,7 +50,7 @@ export default function Three({ spheres }: AppProps) {
   useEffect(() => {
     if (typeof window !== "undefined") {
       const newWidth = window.innerWidth;
-      setAquariumPosition(newWidth <= 1120 ? [0, 0.25, 0] : [0, 0.25, -8]);
+      setAquariumPosition(newWidth <= 1120 ? [0, 0.25, 0] : [6, 0.25, 0]);
       windowWidth.current = newWidth;
 
       const handleResize = () => {
@@ -57,7 +59,7 @@ export default function Three({ spheres }: AppProps) {
           (windowWidth.current! > 1120 && newWidth <= 1120) ||
           (windowWidth.current! <= 1120 && newWidth > 1120)
         ) {
-          setAquariumPosition(newWidth <= 1120 ? [0, 0.25, 0] : [0, 0.25, -8]);
+          setAquariumPosition(newWidth <= 1120 ? [0, 0.25, 0] : [6, 0.25, 0]);
         }
         windowWidth.current = newWidth;
       };
@@ -71,12 +73,13 @@ export default function Three({ spheres }: AppProps) {
   return (
     <Canvas
       shadows
-      camera={{ position: [30, 9, 0], fov: 35, near: 1, far: 40 }}
+      camera={{ position: [0, 5, 30], fov: 35, near: 1, far: 40 }}
     >
-      <color attach="background" args={["#f8fbff"]} />
+      <color attach="background" args={["#f0f0f0"]} />
+      <ambientLight intensity={0.8} />
       <Aquarium position={aquariumPosition}>
         <Float rotationIntensity={2} floatIntensity={10} speed={2}>
-          <Orca position={[0, 0, -1]} rotation={[0, 1, 0]} scale={3} />
+          <Orca position={[1, 0, -1]} rotation={[0, -0.5, 0]} scale={2.5} />
         </Float>
         <Instances renderOrder={-1000}>
           <sphereGeometry args={[1, 64, 64]} />
@@ -92,21 +95,10 @@ export default function Three({ spheres }: AppProps) {
           ))}
         </Instances>
       </Aquarium>
-      <AccumulativeShadows
-        color="#0000ff"
-        colorBlend={0.5}
-        opacity={0.1}
-        scale={60}
-        position={[0, -5, 0]}
-      ></AccumulativeShadows>
+      <Table position={[0, 0, 0]} />
+
       <Environment resolution={1024}>
         <group rotation={[-Math.PI / 3, 0, 0]}>
-          <Lightformer
-            intensity={4}
-            rotation-x={Math.PI / 2}
-            position={[0, 5, -9]}
-            scale={[10, 10, 1]}
-          />
           {Array.from({ length: 8 }, (_, i) => (
             <Lightformer
               key={i}
@@ -123,14 +115,9 @@ export default function Three({ spheres }: AppProps) {
             position={[-5, 1, -1]}
             scale={[50, 2, 1]}
           />
-          <Lightformer
-            intensity={2}
-            rotation-y={-Math.PI / 2}
-            position={[10, 1, 0]}
-            scale={[50, 2, 1]}
-          />
         </group>
       </Environment>
+      <Background/>
     </Canvas>
   );
 }
@@ -155,7 +142,7 @@ function Aquarium({ children, ...props }: AquariumProps) {
     <group {...props} dispose={null}>
       <mesh
         castShadow
-        scale={[3.66, 4.8, 5]}
+        scale={[4, 4, 4]}
         geometry={(nodes.Cube as THREE.Mesh).geometry}
       >
         <MeshTransmissionMaterial
@@ -194,7 +181,7 @@ function Orca(props: OrcaProps) {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const scene = useMemo(() => useGLTF("shiro-syati.glb").scene, []);
   const orcaRef = useRef<THREE.Object3D>(scene);
-  const [showText, setShowText]  = useState<boolean>(false);
+  const [showText, setShowText] = useState<boolean>(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -208,23 +195,48 @@ function Orca(props: OrcaProps) {
 
   return (
     <group {...props}>
-    <primitive object={orcaRef.current} />
-    {showText && (
-      <group position={[0, 0.7, 0]}>
-        <mesh position={[0, 0, -0.01]}>
-          <planeGeometry args={[1.5, 0.4]} />
-          <meshBasicMaterial color="white" transparent opacity={1} />
-        </mesh>
-        <Text
-          fontSize={0.2}
-          color="black"
-          anchorX="center"
-          anchorY="middle"
-        >
-          こんにちわ！
-        </Text>
-      </group>
-    )}
-  </group>
+      <primitive object={orcaRef.current} />
+      {showText && (
+        <group position={[0, 0.7, 0]}>
+          <mesh position={[0, 0, -0.01]}>
+            <planeGeometry args={[1.5, 0.4]} />
+            <meshBasicMaterial color="white" transparent opacity={1} />
+          </mesh>
+          <Text fontSize={0.2} color="black" anchorX="center" anchorY="middle">
+            こんにちわ！
+          </Text>
+        </group>
+      )}
+    </group>
+  );
+}
+
+function Table({ position }: { position: [number, number, number] }) {
+  const woodTexture = useTexture("/wood.webp");
+  return (
+    <group position={position}>
+      <mesh position={[0, -5, 0]} receiveShadow castShadow>
+        <boxGeometry args={[30, 2, 20]} /> {/* 幅8, 厚さ0.2, 奥行き6 */}
+        <meshStandardMaterial
+          map={woodTexture}
+          roughness={0.9}
+          metalness={0.01}
+        />
+      </mesh>
+    </group>
+  );
+}
+
+function Background() {
+  const texture = useTexture("./books.webp");
+  return (
+    <mesh position={[0, 0, -5]} rotation={[0, 0, 0]}> {/* z を -30 に変更 */}
+      <planeGeometry args={[50, 30, 1]} /> {/* サイズを大きくして画面を覆う */}
+      <meshStandardMaterial
+        map={texture as THREE.Texture}
+        depthTest={true} // 他のオブジェクトと衝突しないようにする
+        transparent
+      />
+    </mesh>
   );
 }
